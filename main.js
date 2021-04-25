e8fd7c5e9c4cf10dd76e92ad4aebb6f66652c27d
xiao_e_yun <67814228+QAZXSW517@users.noreply.github.com>
2021-04-25 17:01:03 +0800

    修復壁紙影引擎監聽器首次更新比DOM加載更快導致無法進行啟動加載

--- main.js
+++ main.js
@@ -118,162 +118,172 @@ window.panel = {
         }
     },
 };
+
+window.requestAnimationFrame(fx.fps.run);
+//================================創建面板================================
+panel.creat("clock") //時鐘
+panel.creat("cal") //日曆
+//==================================監聽==================================
+window.wallpaperPropertyListener = {
+    //▲-------------------------監聽暫停-------------------------▲
+    setPaused: function (isPaused) {
+        if (!isPaused) {//重新啟動
+            timer.set()
+        } else { //已暫停
+
+        }
+    },
+    //▲-------------------------監聽系統設定-------------------------▲
+    applyGeneralProperties: function (setting) {
+        if (setting.fps) {
+            fx.fps.fixed = setting.fps;
+        }
+
+    },
+    //▲-------------------------監聽用戶設定-------------------------▲
+    applyUserProperties: function (user) {
+        if (window.fx.dom) {//等待DOM加載完成
+            window.apply_setting(user)
+        } else {
+            console.log("DOM need ready!")
+            $(() => { window.apply_setting(user) })
+        }
+    }
+};
+
 $(() => {
-    window.requestAnimationFrame(fx.fps.run);
-    //================================創建面板================================
-    panel.creat("clock") //時鐘
-    panel.creat("cal") //日曆
-    //==================================監聽==================================
-    window.wallpaperPropertyListener = {
-        //▲-------------------------監聽暫停-------------------------▲
-        setPaused: function (isPaused) {
-            if (!isPaused) {//重新啟動
-                timer.set()
-            } else { //已暫停
+    window.fx.dom = true
+    window.apply_setting = function (user) {
+        //==================================背景==================================
+        body = $("body")
 
+        function change_bg() {
+            body.css({
+                "background-image": "",
+                "background-color": ""
+            })
+            if (bg.video_tmp) {
+                $("#bg_video").remove()
+                bg.video_tmp = false
             }
-        },
-        //▲-------------------------監聽系統設定-------------------------▲
-        applyGeneralProperties: function (setting) {
-            if (setting.fps) {
-                fx.fps.fixed = setting.fps;
-            }
-
-        },
-        //▲-------------------------監聽用戶設定-------------------------▲
-        applyUserProperties: function (user) {
-
-            //==================================背景==================================
-            body = $("body")
-
-            function change_bg() {
-                body.css({
-                    "background-image": "",
-                    "background-color": ""
-                })
-                if (bg.video_tmp) {
-                    $("#bg_video").remove()
-                    bg.video_tmp = false
-                }
-                switch (bg.type) {
-                    case ("color"):
-                        body.css({
-                            "background-color": bg.color
-                        })
-                        break;
-                    case ("image"):
-                        body.css({
-                            "background-image": "url('" + (
-                                (bg.img_type == "file") ?
-                                    bg.img_file :
-                                    bg.img_url
-                            ) + "')"
-                        })
-                        break;
-                    case ("video"):
-                        if (bg.video_file) {
-                            if (!bg.video_tmp) {
-                                $("body").append("<video id=\"bg_video\" src=\"\" loop autoplay></video>")
-                                bg.video_tmp = true
-                            }
-                            $("#bg_video")[0].src = "file:///" + bg.video_file
+            switch (bg.type) {
+                case ("color"):
+                    body.css({
+                        "background-color": bg.color
+                    })
+                    break;
+                case ("image"):
+                    body.css({
+                        "background-image": "url('" + (
+                            (bg.img_type == "file") ?
+                                bg.img_file :
+                                bg.img_url
+                        ) + "')"
+                    })
+                    break;
+                case ("video"):
+                    if (bg.video_file) {
+                        if (!bg.video_tmp) {
+                            $("body").append("<video id=\"bg_video\" src=\"\" loop autoplay></video>")
+                            bg.video_tmp = true
                         }
-                        break;
-                    case ("bing_api"):
-                        $.getJSON("https://bing.biturl.top/", (json) => {
-                            body.css({
-                                "background-image": "url('" + json.url + "')"
-                            })
+                        $("#bg_video")[0].src = "file:///" + bg.video_file
+                    }
+                    break;
+                case ("bing_api"):
+                    $.getJSON("https://bing.biturl.top/", (json) => {
+                        body.css({
+                            "background-image": "url('" + json.url + "')"
                         })
-                        break;
+                    })
+                    break;
+            }
+        }
+        if (user.background_type) {
+            bg.type = user.background_type.value
+            change_bg()
+        }
+        if (user.background_color) {
+            bg.color = get_color(user.background_color.value, true)
+            change_bg()
+        }
+        if (user.background_image_type) {
+            bg.img_type = user.background_image_type.value
+            change_bg()
+        }
+        if (user.background_image_file) {
+            let val = user.background_image_file.value
+            bg.img_file = (val == "") ? ("background.webp") : ("file:///" + val)
+            change_bg()
+        }
+        if (user.background_image_url) {
+            let val = user.background_image_url.value
+            if (val.indexOf("https://") === -1 && val.indexOf("http://") === -1) {
+                val = "https://" + val
+            }
+            bg.img_url = val
+            change_bg()
+        }
+        if (user.background_video_file) {
+            bg.video_file = unescape(user.background_video_file.value)
+            change_bg()
+        }
+        if (user.background_size) {
+            body.css({ "background-size": user.background_size.value })
+        }
+        //==================================特效==================================
+        if (user.fx_sakura$type) {
+            let val = user.fx_sakura$type.value
+            if (val) {
+                if (!fx.sakura.tmp) {
+                    $.get("sakura/shader.html", (data) => {
+                        $("body").append(data)
+                        sakura_onload()
+                    })
+                    window.fx.sakura.tmp = true
                 }
             }
-            if (user.background_type) {
-                bg.type = user.background_type.value
-                change_bg()
-            }
-            if (user.background_color) {
-                bg.color = get_color(user.background_color.value, true)
-                change_bg()
-            }
-            if (user.background_image_type) {
-                bg.img_type = user.background_image_type.value
-                change_bg()
-            }
-            if (user.background_image_file) {
-                let val = user.background_image_file.value
-                bg.img_file = (val == "") ? ("background.webp") : ("file:///" + val)
-                change_bg()
-            }
-            if (user.background_image_url) {
-                let val = user.background_image_url.value
-                if (val.indexOf("https://") === -1 && val.indexOf("http://") === -1) {
-                    val = "https://" + val
-                }
-                bg.img_url = val
-                change_bg()
-            }
-            if (user.background_video_file) {
-                bg.video_file = unescape(user.background_video_file.value)
-                change_bg()
-            }
-            if (user.background_size) {
-                body.css({ "background-size": user.background_size.value })
-            }
-            //==================================特效==================================
-            if (user.fx_sakura$type) {
-                let val = user.fx_sakura$type.value
-                if (val) {
-                    if (!fx.sakura.tmp) {
-                        $.get("sakura/shader.html", (data) => {
-                            $("body").append(data)
-                            sakura_onload()
+            $("#sakura")[val ? "fadeIn" : "fadeOut"]()
+            window.fx.sakura.type = val
+            fx.sakura.chg_opc()
+        }
+        if (user.fx_sakura$opc) {
+            window.fx.sakura.opacity = user.fx_sakura$opc.value
+            fx.sakura.chg_opc()
+        }
+
+        const $key = Object.keys(user)
+        if ($key.some((t) => { return t.indexOf("$") != -1 })) {
+            // ============================================================================================
+            //                                   ※switch 大型搜尋設置
+            // ============================================================================================
+            for (const [$key, $val] of Object.entries(user)) {
+                //==================================聲音可視化==================================Z
+                let audio = "audio_visualization$"
+                if ($key.indexOf(audio) === 0) {//驗證是否為聲音可視化
+                    let main = $key.slice(audio.length)
+                    let val = $val.value
+                    if (main === "type" && val === true && !audv.tmp) {
+                        $.getScript("audio_visualization/index.js", () => {
+                            audv.tmp = true
+                            audv.reload()
                         })
-                        window.fx.sakura.tmp = true
                     }
+                    audv.opt[main] = val
+                    if (audv.tmp) { audv.set(main); }
+                    // 在json添加| audio_visualization$類型 |
                 }
-                $("#sakura")[val ? "fadeIn" : "fadeOut"]()
-                window.fx.sakura.type = val
-                fx.sakura.chg_opc()
-            }
-            if (user.fx_sakura$opc) {
-                window.fx.sakura.opacity = user.fx_sakura$opc.value
-                fx.sakura.chg_opc()
-            }
 
-            const $key = Object.keys(user)
-            if ($key.some((t) => { return t.indexOf("$") != -1 })) {
-                // ============================================================================================
-                //                                   ※switch 大型搜尋設置
-                // ============================================================================================
-                for (const [$key, $val] of Object.entries(user)) {
-                    //==================================聲音可視化==================================Z
-                    let audio = "audio_visualization$"
-                    if ($key.indexOf(audio) === 0) {//驗證是否為聲音可視化
-                        let main = $key.slice(audio.length)
-                        let val = $val.value
-                        if (main === "type" && val === true && !audv.tmp) {
-                            $.getScript("audio_visualization/index.js", () => {
-                                audv.tmp = true
-                                audv.reload()
-                            })
-                        }
-                        audv.opt[main] = val
-                        if (audv.tmp) { audv.set(main); }
-                        // 在json添加| audio_visualization$類型 |
-                    }
-
-                    //==================================面板==================================
-                    if ($key.indexOf("panel_") === 0) {//驗證是否為面板
-                        let key = panel.RegExp.exec($key).groups
-                        let val = $val.value
-                        panel.RegExp.lastIndex = 0;
-                        panel.set(key.panel, key.type, val)
-                    }
+                //==================================面板==================================
+                if ($key.indexOf("panel_") === 0) {//驗證是否為面板
+                    let key = panel.RegExp.exec($key).groups
+                    let val = $val.value
+                    panel.RegExp.lastIndex = 0;
+                    panel.set(key.panel, key.type, val)
                 }
             }
         }
-    };
+    }
 })
 //==================================函式==================================
 
